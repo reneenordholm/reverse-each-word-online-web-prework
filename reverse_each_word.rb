cabb8445973833cb6c3cf68d9ec0468a3aa72ff3
Renee Nordholm <reneenordholm@gmail.com>
2019-03-30 21:52:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,10 @@ def reverse_each_word(sentence1)
 end
 
 def reverse_each_word(sentence2)
-
+  array = sentence2.split(" ")
+  test_array = []
+  array.collect do|sentence2| 
+    test_array << sentence2.reverse 
+  end
+  test_array.join(" ")
 end
