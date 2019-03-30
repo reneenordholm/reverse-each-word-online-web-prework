0a95d80e26523e1f77cffec2d62e874121478048
Renee Nordholm <reneenordholm@gmail.com>
2019-03-30 21:51:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(sentence1)
   original_array = sentence1.split(" ")
-return_array = []
-original_array.each do|sentence1|
+  return_array = []
+  original_array.each do|sentence1|
   return_array << sentence1.reverse
-end
-return_array.join(" ")
+ end
+  return_array.join(" ")
 end
 
-#def reverse_each_word(sentence1)
-#  sentence1.split
-#end
+def reverse_each_word(sentence2)
+
+end
