2e10469b75ebafebe278089223263a2910133ce8
Renee Nordholm <reneenordholm@gmail.com>
2019-03-30 21:49:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence1)
   original_array = string.split(" ")
 return_array = []
-original_array.each do|string|
-  return_array << string.reverse
+original_array.each do|sentence1|
+  return_array << sentence1.reverse
 end
 return_array.join(" ")
 end
